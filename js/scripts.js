f3bccaf8e288e0f6d5d16838a889b7822f675d92
kristofkania33 <zyhkana25@wp.pl>
2017-06-01 13:05:06 +0200

    loop

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+//scripts.js
+
+function drawTree(pietra) {
+	for(var i=1;i<=pietra;i++) {
+		var star = '';
+		//rysowanie gwiazdek
+		for(var j=1;j<=i;j++) {
+			star = star + " *   ";
+		}
+		console.log(star);
+	}
+}
+
+drawTree(5)
